cea6186f7aaca84057c6058dfa48073322336ea0
ijd1236 <130967884+ijd1236@users.noreply.github.com>
2024-02-05 21:58:02 +0900

    a

--- python_study/dlt.py
+++ python_study/dlt.py
@@ -10,3 +10,21 @@ def read_dlt_file(file_path):
 if __name__ == "__main__":
     dlt_file_path = "bbb.dlt"
     read_dlt_file(dlt_file_path)
+
+
+
+
+
+import os
+import dlt
+
+def read_dlt_file(file_path):
+    with dlt.File(file_path) as file:
+        for message in file:
+            # 메시지의 텍스트 필드에서 "kactivating"을 포함한 메시지만 출력
+            if "kactivating" in message.text:
+                print(message)
+
+if __name__ == "__main__":
+    dlt_file_path = "bbb.dlt"
+    read_dlt_file(dlt_file_path)
